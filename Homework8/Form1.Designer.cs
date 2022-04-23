7fbc649483b72fa524794d44e64fd0b310bb45fa
Howie Chen <howie.c@outlook.com>
2022-04-23 23:18:08 +0800

    Add homework8

---
+++ Homework8/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Homework8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(268, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Baidu";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(479, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(497, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "百度一下";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 149);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(277, 200);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(295, 149);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(277, 200);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Homework8/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.XPath;
+using System.Xml;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Homework8
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string wd = textBox1.Text;
+            Thread thread1 = new Thread(() => Search("textBox2", wd));
+            Thread thread2 = new Thread(() => Search("textBox3", wd));
+            thread1.Start();
+            thread2.Start();
+        }
+
+        public void Print(string boxName, List<string> list)
+        {
+            if (boxName == "textBox2")
+            {
+                foreach (string item in list)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        textBox2.AppendText(item);
+                        textBox2.AppendText(System.Environment.NewLine);
+                    }));
+                }
+            }
+            else
+            {
+                foreach (string item in list)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        textBox3.AppendText(item);
+                        textBox3.AppendText(System.Environment.NewLine);
+                    }));
+                }
+            }
+        }
+
+        public void Search(string boxName, string wd)
+        {
+            string url = "https://www.baidu.com/s?wd=" + wd;
+            string html = string.Empty;
+            string xpathDetectRegex = @"<h3(\s|\S)*?>(\s|\S)*?</h3>";
+            string xpathURLRegex = @"(href|HREF)[]*=[]*[""'](?<url>[^""'#>]+)[""']";
+            string xpathTitleRegex = @"<a[\s\S]*>(?<title>[\s\S]*)</a>";
+
+            try
+            {
+                WebClient webClient = new WebClient();          
+                //webClient.Headers.Add("User-Agent", "Mozilla / 5.0(Android; Mobile; rv: 58.0) Gecko / 58.0 Firefox / 58.0");
+                webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36");
+                webClient.Encoding = Encoding.UTF8;
+                html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+            }
+
+            MatchCollection matches = new Regex(xpathDetectRegex).Matches(html);
+            string title = string.Empty;
+            string href = string.Empty;
+            List<string> list = new List<string>();
+            foreach (Match match in matches)
+            {
+                title = Regex.Match(match.Value, xpathTitleRegex).Groups["title"].Value;
+                href = Regex.Match(match.Value, xpathURLRegex).Groups["url"].Value;
+                list.Add(title);
+                list.Add(href);
+            }
+            Print(boxName, list);
+        }
+    }
+}
